873c0902c12b96f7e09e0094c25a29e15177ed14
inicioprogramadorcoffe <inicioprogramador216@gmail.com>
2026-02-14 14:33:13 -0500

    cadenas

---
+++ CursoPy/cadenas.py
@@ -0,0 +1,25 @@
+# PI = 3.1416
+# print('El valor de Pi es: ', PI)
+
+# print()
+
+# NOMBRE_BASE_DATOS = "Tienda_Online_db"
+# print('Nombre bade de datos: ', NOMBRE_BASE_DATOS)
+
+libro = "Leí el libro 'Return to Moria'"
+#Usando comilla triple
+menu = """
+MENÚ DEL DÍA
+----------------------
+1. Tacos peruanos mexicanos
+2. Enchiladas mexicanas
+3. Aguacate al horno
+"""
+print(menu)
+print()
+#Error ~> mensaje = "Ella dijo "hola" al entrar"
+mensaje = "Ella dijo \"hola\" al entrar"
+print(mensaje)
+
+mensaje = "Ella \nes mala"
+print(mensaje)
