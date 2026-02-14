c663a1b3f67bf8ed184db087538ec63e2644318c
inicioprogramadorcoffe <inicioprogramador216@gmail.com>
2026-02-14 12:37:43 -0500

    cambio_Practica

---
+++ CursoPy/reservaHotel.py
@@ -0,0 +1,14 @@
+#Sistema de Reserva de hotel
+nombre_cliente = 'pablo morza'
+Dias_estancia = 10
+tarifa_diaria = 20000
+ventana_mar = True
+valorT_dias = Dias_estancia * tarifa_diaria
+
+print('***Sistema de Reserva de Hotel***')
+print('Cliente: ', nombre_cliente)
+print('Día de estancia: ', Dias_estancia)
+print('Valor por día: ', tarifa_diaria)
+print('Habitación con vista al mar?: ', ventana_mar)
+print('Valor total por dias de estancia: ', valorT_dias)
+#commit
---
+++ CursoPy/variabeles.py
@@ -0,0 +1,10 @@
+nombre_explorador = "juan cipriano"
+planeta_origen = 'Pluton'
+edad_explorador = 28
+misiones_completadas = 4 
+nivel_energia = 87.5
+
+print(nombre_explorador)
+print("Planeta de origen es: ", planeta_origen)
+
+
