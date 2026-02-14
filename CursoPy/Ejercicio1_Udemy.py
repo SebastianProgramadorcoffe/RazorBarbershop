f4aa42cad7c28f710a22efecb45dabf203b90709
inicioprogramadorcoffe <inicioprogramador216@gmail.com>
2026-02-14 13:53:14 -0500

    inventario1

---
+++ CursoPy/Ejercicio1_Udemy.py
@@ -0,0 +1,29 @@
+#Sistema de inventario de productos
+nombre_producto = 'Reloj Digital corsair tecnology'
+precio_producto = 268000
+cantidad_producto = 5
+disponivilidad = cantidad_producto > 0 
+print('--------------------------------------------')
+print('***Sistema de Inventario***')
+print('--------------------------------------------')
+print('Articulo: ', nombre_producto)
+print('Precio: $', precio_producto)
+print('Cantidad disponible:', cantidad_producto)
+print('Disponible: ', disponivilidad)
+print('--------------------------------------------')
+
+#Modificación por variación de producto
+print()
+print()
+nombre_producto = 'Reloj Digítal Bluetooth Amper 1 '
+precio_producto = 374000
+cantidad_producto = 0
+disponivilidad = cantidad_producto > 0 
+print('--------------------------------------------')
+print('***Sistema de Inventario***')
+print('--------------------------------------------')
+print('Articulo: ', nombre_producto)
+print('Precio: $', precio_producto)
+print('Cantidad disponible:', cantidad_producto)
+print('Disponible: ', disponivilidad)
+print('--------------------------------------------')
--- CursoPy/reservaHotel.py
+++ CursoPy/reservaHotel.py
@@ -12,3 +12,17 @@ print('Valor por día: ', tarifa_diaria)
 print('Habitación con vista al mar?: ', ventana_mar)
 print('Valor total por dias de estancia: ', valorT_dias)
 #commit
+#Modificaciones para nuevo cliente
+nombre_cliente = 'pablo morza'
+Dias_estancia = 8
+tarifa_diaria = 15000
+ventana_mar = False
+valorT_dias = Dias_estancia * tarifa_diaria
+print()
+print('_______________________________________________')
+print('***Sistema de Reserva de Hotel***')
+print('Cliente: ', nombre_cliente)
+print('Día de estancia: ', Dias_estancia)
+print('Valor por día: ', tarifa_diaria)
+print('Habitación con vista al mar?: ', ventana_mar)
+print('Valor total por dias de estancia: ', valorT_dias)
