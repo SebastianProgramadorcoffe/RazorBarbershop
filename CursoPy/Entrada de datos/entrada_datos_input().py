fcdade78d92b8877b01b96b8b455d34da5ad466e
inicioprogramadorcoffe <inicioprogramador216@gmail.com>
2026-02-16 18:44:57 -0500

    cambios

--- CursoPy/Entrada de datos/entrada_datos_input().py
+++ CursoPy/Entrada de datos/entrada_datos_input().py
@@ -2,5 +2,13 @@
 #  creando una variable de almacenamiento posterior
 #  a esto el mensaje que vera el usuario
 # 
-entrada_1 = input("Nombre de Usuario: " )
+entrada_1 = input("Nombre de Usuario: ")
 print(entrada_1) 
+#Cuando ingresamos datos, 
+# los datos se alamacenan como string, para pasar a otro tipo de datos de
+# cadena a int se agrega 
+# la entrada dento del los parametros de int() = int(input("Ingrese su Nombre: "))
+almacen_entrada = int(input("Ingrese su edad: "))
+
+print(f"Tu edad es: {almacen_entrada}")
+
