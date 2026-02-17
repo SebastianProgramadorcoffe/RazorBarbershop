7595e8461ba5b5e89a40c22da281a07f7911150b
inicioprogramadorcoffe <inicioprogramador216@gmail.com>
2026-02-17 18:23:23 -0500

    generador de placa

---
+++ CursoPy/Sistema_generador_de_id.py
@@ -0,0 +1,19 @@
+from random import randint
+print("***Sistema generador de Id Unico***")
+#ingreso de los datos principales primer y segundo nombre mas añode nacimiento
+primer_nombre = input('Ingrese Primer Nombre: ')
+segundo_nombre = input('Ingrese Segundo Nombre: ')
+ano_nacimiento = input('Ingrese su año de nacimiento(yyyy): ')
+#alamacenamos y pasamos los 2 primeros caracteres de 
+# prnm y sgnm a mayusculas al igual que tomar 2 letras del año de nacimiento
+dos_primern = primer_nombre.upper()[0:2]
+dos_segundon = segundo_nombre.upper()[0:2]
+dos_anonaci = ano_nacimiento[2:4]
+numeros_aleatorios = randint(1001, 9999)
+#realizamos la concatenacion de los datos
+numero_de_placa = f"{dos_primern}{dos_segundon}{dos_anonaci}{numeros_aleatorios}"
+
+print("***Numero de placa del vehiculo***")
+print("_______________________________________")
+print(f"Hola {primer_nombre},")
+print(f"    Tu numero de (Placa)) es: {numero_de_placa}\nFelicidades!")
