c9e88a81c03d36ac9faf3b437d56f907a8a8342c
inicioprogramadorcoffe <inicioprogramador216@gmail.com>
2026-02-17 18:29:44 -0500

    mejora

--- CursoPy/Sistema_generador_de_id.py
+++ CursoPy/Sistema_generador_de_id.py
@@ -6,9 +6,10 @@ segundo_nombre = input('Ingrese Segundo Nombre: ')
 ano_nacimiento = input('Ingrese su año de nacimiento(yyyy): ')
 #alamacenamos y pasamos los 2 primeros caracteres de 
 # prnm y sgnm a mayusculas al igual que tomar 2 letras del año de nacimiento
-dos_primern = primer_nombre.upper()[0:2]
-dos_segundon = segundo_nombre.upper()[0:2]
-dos_anonaci = ano_nacimiento[2:4]
+#.strip() para quitar espacios al inicio y al final
+dos_primern = primer_nombre.strip().upper()[0:2]
+dos_segundon = segundo_nombre.strip().upper()[0:2]
+dos_anonaci = ano_nacimiento.strip()[2:4]
 numeros_aleatorios = randint(1001, 9999)
 #realizamos la concatenacion de los datos
 numero_de_placa = f"{dos_primern}{dos_segundon}{dos_anonaci}{numeros_aleatorios}"
